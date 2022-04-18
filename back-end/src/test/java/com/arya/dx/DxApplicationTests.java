d5e3d6eb07711ea38bbd3d85e6d1e4d096394752
Diarno Tall <diarnotall@gmail.com>
2022-04-18 10:05:06 -0700

    Add Spring boot project

---
+++ back-end/src/test/java/com/arya/dx/DxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arya.dx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
